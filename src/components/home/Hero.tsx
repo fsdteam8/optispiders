ef16ebf098d575c56a34db249ca4f20b49fa283a
Monir Hossain <monirhrabby.personal@gmail.com>
2025-08-20 16:20:52 +0600

    fix: banner

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,26 +1,24 @@
 import { images } from "@/constants/image";
 import { ArrowRight, Star } from "lucide-react";
-import React from "react";
-import { Button } from "../ui/button";
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
     <div
+      className="relative min-h-[70vh] h-auto py-[100px] text-white flex flex-col items-center justify-center bg-center bg-cover"
       style={{
-        backgroundImage: `url(${images.banner})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundBlendMode: "overlay",
-        backgroundColor: "#38303084",
+        backgroundImage: `url(https://files.edgestore.dev/l2c0thic6lofxtc5/optispiders/_public/banner.webp)`,
       }}
-      className="min-h-[70vh] text-white flex flex-col items-center justify-center"
     >
-      <div className="container flex flex-col gap-6 lg:flex-row justify-between items-center">
+      {/* Overlay layer */}
+      <div className="absolute inset-0 bg-white/5" />
+      {/* adjust /30 → /20 or /40 for more/less transparency */}
+
+      <div className="container relative z-10 flex flex-col gap-6 lg:flex-row justify-between items-center">
         <div>
-          <h1 className="flex items-center gap-2 text-[#4299e1]">
-            <Star /> Smarter Retail Starts Here
+          <h1 className="flex items-center text-[14px] gap-2 text-[#4299e1]">
+            <Star className="h-4 w-4" /> Smarter Retail Starts Here
           </h1>
 
           <h1 className="my-5 text-4xl font-semibold max-w-2xl">
@@ -28,30 +26,31 @@ const Hero = () => {
             with <span className="text-blue-500">AI-powered</span> solutions.
           </h1>
 
-          <p className=" lg:max-w-2xl">
+          <p className="lg:max-w-2xl">
             OptiSpiders empowers leading brands to scale sales and outpace the
             competition using advanced AI, data-driven SEO, compelling
             content,and strategic marketplace optimization.
           </p>
 
-          <div className=" space-x-5 mt-8">
-            <Button>
+          <div className="space-x-5 mt-8">
+            <Button className="bg-[#2793D7] hover:bg-[#2793D7]/80">
               Get Started <ArrowRight />
             </Button>
-            <Button className="bg-inherit border border-white text-white">
+            <Button className="bg-inherit border hover:bg-white/20 border-white/50 text-white">
               View Products
             </Button>
           </div>
         </div>
 
         <div>
-          <div>
+          <div className="w-full max-w-[535px]">
             <Image
               src={images.heroImg}
-              alt="hero.png"
-              width={1000}
-              height={1000}
-              className="h-[450px] w-[450px] rounded-lg shadow-lg"
+              alt="Hero image"
+              width={535}
+              height={535}
+              className="w-full h-auto rounded-lg shadow-lg"
+              priority
             />
           </div>
         </div>

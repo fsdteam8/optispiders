c00693d0e69d0a505f88a4bc408579e8974c1b32
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-08-21 04:42:50 +0600

    ManageMedia, ServicePaidSearch & Footer Reuseable commponent Done

--- src/app/services/managed-media/page.tsx
+++ src/app/services/managed-media/page.tsx
@@ -1,5 +1,26 @@
+import Hero from "@/components/home/Hero";
+import ManagedMedia from "@/components/shared/ManagedMedia";
+import ServicePaidSearch from "@/components/shared/ServicePaidSearch";
+import paidSearchImg from "../../../../public/service/service_Image1.png";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <Hero />
+      <ManagedMedia
+        subtitle="Expert Services"
+        title="Managed Media"
+        description="Our Managed Media service at Optispider is designed to drive real results across major online retail platforms. We take a hands-on approach to campaign management—combining data, automation, and strategic planning to make sure your products appear in front of the right shoppers at the perfect time. Our focus is on growing your visibility, boosting conversions, and improving return on ad spend (ROAS), all while keeping your advertising budget efficient and effective."
+      />
+
+      <ServicePaidSearch
+        title="Paid Search"
+        description="Search ads are a powerful tool to reach shoppers who are already looking for what you are offering. We identify high-performing keywords, optimize product listings, and actively manage bids to ensure your products rank competitively in search results. By analyzing traffic patterns and shopper behavior, we position your listings for maximum exposure and strong sales performance."
+        image={paidSearchImg}
+        link="#"
+      />
+    </div>
+  );
 };
 
 export default Page;
--- src/app/services/retail-excellence/page.tsx
+++ src/app/services/retail-excellence/page.tsx
@@ -1,5 +1,27 @@
+import ManagedMedia from "@/components/shared/ManagedMedia";
+import ServicePaidSearch from "@/components/shared/ServicePaidSearch";
+import service_image_3 from "../../../../public/service/service_image_3.png"
+import { Globe } from "lucide-react";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <ManagedMedia
+        subtitle="Retail Excellence Services"
+        title="Retail Excellence"
+        description="Retail Excellence is about more than just sales—it’s about creating seamless shopping experiences, building lasting customer relationships, and setting new standards of service. By combining innovation, efficiency, and insight, it empowers businesses to thrive in a competitive marketplace."
+      />
+
+      <ServicePaidSearch
+        title="Retail Excellence"
+        description="At Optispider, Retail Excellence means ensuring your business runs smoothly, efficiently, and profitably across online marketplaces. We go beyond sales performance focusing on the integrity of your brand, operational compliance, and financial accuracy. With our team monitoring the backend as closely as the front end, you gain peace of mind and better profitability."
+        image={service_image_3}
+        link="#"   
+        subtitle={"Expert Services"} 
+        icon={<Globe size={20} />}
+      />
+    </div>
+  );
 };
 
 export default Page;

5fd48735e07f3f55013a7f761dd9f88cb2266d8a
Monir Hossain <monirhrabby.personal@gmail.com>
2025-08-21 09:48:33 +0600

    a

--- src/app/(website)/services/managed-media/page.tsx
+++ src/app/(website)/services/managed-media/page.tsx
@@ -1,5 +1,15 @@
+import ManagedMedia from "@/components/shared/ManagedMedia";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <ManagedMedia
+        subtitle="Expert Services"
+        title="Managed Media"
+        description="Our Managed Media service at Optispider is designed to drive real results across major online retail platforms. We take a hands-on approach to campaign management—combining data, automation, and strategic planning to make sure your products appear in front of the right shoppers at the perfect time. Our focus is on growing your visibility, boosting conversions, and improving return on ad spend (ROAS), all while keeping your advertising budget efficient and effective."
+      />
+    </div>
+  );
 };
 
 export default Page;

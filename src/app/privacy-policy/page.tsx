8416d95dc33a482d387e2576a6f80cdae67feb37
iftykhar <s.m.ifty49@gmail.com>
2025-08-22 02:39:14 +0600

    minor change

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import Privacypolicy from "@/components/privacy-policy/Privacypolicy";
+import ReadyToTransform from "@/components/shared/ReadyToTransform";
+
+const Page = () => {
+  return(
+    <div>
+      <Privacypolicy />
+      <ReadyToTransform />
+    </div>
+  )
+};
+
+export default Page;

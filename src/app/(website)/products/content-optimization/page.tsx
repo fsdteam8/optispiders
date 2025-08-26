a4cd7878071dbac3ee2a7281e7b016a16be6bedd
ILhankhondaker <ilhankhondaker@gmail.com>
2025-08-26 10:19:41 +0600

    fix image issue

--- src/app/(website)/products/content-optimization/page.tsx
+++ src/app/(website)/products/content-optimization/page.tsx
@@ -1,7 +1,7 @@
 // import Hero from '@/components/home/Hero'
 import ServicePaidSearch from '@/components/shared/ServicePaidSearch'
 import productsPage1 from '../../../../../public/product/productPage2_1.png'
-import productsPage3 from '../../../../../public/product/ChatGPTImage.png'
+import productsPage3 from '../../../../../public/product/xyz.png'
 import RatailMediaNetwork from '@/components/shared/RatailMediaNetwork'
 import ReadyToTransform from '@/components/shared/ReadyToTransform'
 import VisibilityIntelligence from '@/components/visibiliyIntelligence/VisibilityIntelligence'
